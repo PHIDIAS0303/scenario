120c648e33e80185f019a2c8f53cf8f5a1da47eb
PHIDIAS <phidias@aperx.org>
2024-06-20 20:42:20 +0900

    .

--- config/bonus.lua
+++ config/bonus.lua
@@ -49,134 +49,194 @@ return {
             max = 500,
             scale = 50,
             cost = 0.01
-        }
+        },
+        ['character_reach_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
+        ['character_resource_reach_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
+        ['character_build_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
         --[[
-        {
-            name = 'character_reach_distance_bonus',
-            value = 10
+        ['character_item_pickup_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         },
-        {
-            name = 'character_resource_reach_distance_bonus',
-            value = 10
+        ['character_loot_pickup_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         },
-        {
-            name = 'character_build_distance_bonus',
-            value = 10
-        },
-        {
-            name = 'character_item_pickup_distance_bonus',
-            value = 0
-        },
-        {
-            name = 'character_loot_pickup_distance_bonus',
-            value = 0
-        },
-        {
-            name = 'character_item_drop_distance_bonus',
-            value = 0
+        ['character_item_drop_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         }
         ]]
     },
     force_bonus = {
-        {
-            name = 'manual_mining_speed_modifier',
-            value = 2
+        ['character_mining_speed_modifier'] = {
+            value = 2,
+            max = 4,
+            scale = 0.25,
+            cost = 10
         },
-        {
-            name = 'character_running_speed_modifier',
-            value = 1
+        ['character_running_speed_modifier'] = {
+            value = 1.5,
+            max = 3,
+            scale = 0.25,
+            cost = 20
         },
-        {
-            name = 'character_crafting_speed_modifier',
-            value = 5
+        ['character_crafting_speed_modifier'] = {
+            value = 5,
+            max = 10,
+            scale = 0.5,
+            cost = 10
         },
-        {
-            name = 'character_inventory_slots_bonus',
-            value = 100
+        ['character_inventory_slots_bonus'] = {
+            value = 80,
+            max = 120,
+            scale = 10,
+            cost = 1
         },
-        {
-            name = 'character_health_bonus',
-            value = 200
+        ['character_health_bonus'] = {
+            value = 200,
+            max = 500,
+            scale = 50,
+            cost = 0.01
+        },
+        ['character_reach_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
+        ['character_resource_reach_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
+        ['character_build_distance_bonus'] = {
+            value = 10,
+            max = 20,
+            scale = 1,
+            cost = 1
+        },
+        ['worker_robots_speed_modifier'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
+        },
+        ['worker_robots_battery_modifier'] = {
+            value = 1,
+            max = 1,
+            scale = 1,
+            cost = 1
+        },
+        ['worker_robots_storage_bonus'] = {
+            value = 1,
+            max = 1,
+            scale = 1,
+            cost = 1
+        },
+        ['following_robots_lifetime_modifier'] = {
+            value = 1,
+            max = 1,
+            scale = 1,
+            cost = 1
         },
         --[[
-        {
-            name = 'character_reach_distance_bonus',
-            value = 10
+        ['character_item_pickup_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         },
-        {
-            name = 'character_resource_reach_distance_bonus',
-            value = 10
+        ['character_loot_pickup_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         },
-        {
-            name = 'character_build_distance_bonus',
-            value = 10
+        ['character_item_drop_distance_bonus'] = {
+            value = 0,
+            max = 20,
+            scale = 1,
+            cost = 1
         },
-        {
-            name = 'character_item_pickup_distance_bonus',
-            value = 0
+        ['character_trash_slot_count'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'character_loot_pickup_distance_bonus',
-            value = 0
+        ['mining_drill_productivity_bonus'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'character_item_drop_distance_bonus',
-            value = 5
+        ['train_braking_force_bonus'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        ]]
-        {
-            name = 'worker_robots_speed_modifier',
-            value = 0
+        ['laboratory_speed_modifier'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'worker_robots_battery_modifier',
-            value = 1
+        ['laboratory_productivity_bonus'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'worker_robots_storage_bonus',
-            value = 1
+        ['inserter_stack_size_bonus'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'following_robots_lifetime_modifier',
-            value = 1
+        ['stack_inserter_capacity_bonus'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         },
-        {
-            name = 'character_trash_slot_count',
-            value = 0
-        },
-        {
-            name = 'mining_drill_productivity_bonus',
-            value = 0
-        },
-        {
-            name = 'train_braking_force_bonus',
-            value = 0
-        },
-        {
-            name = 'laboratory_speed_modifier',
-            value = 0
-        },
-        {
-            name = 'laboratory_productivity_bonus',
-            value = 0
-        },
-        {
-            name = 'inserter_stack_size_bonus',
-            value = 0
-        },
-        {
-            name = 'stack_inserter_capacity_bonus',
-            value = 0
-        },
-        {
-            name = 'artillery_range_modifier',
-            value = 0
+        ['artillery_range_modifier'] = {
+            value = 0,
+            max = 0,
+            scale = 0,
+            cost = 1
         }
+        ]]
     },
     surface_bonus = {
-        {
-            name = 'solar_power_multiplier',
-            value = 0
+        ['solar_power_multiplier'] = {
+            value = 0,
+            max = 1000,
+            scale = 1,
+            cost = 1
         }
     }
 }
