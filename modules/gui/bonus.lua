f59e9affb68e79c274940d027bb6e3435e1dfc10
PHIDIAS <phidias@aperx.org>
2024-07-29 18:00:37 +0900

    .

--- modules/gui/bonus.lua
+++ modules/gui/bonus.lua
@@ -265,6 +265,13 @@ Gui.element(function(definition, parent)
     bonus_control_set(container, 'bonus_st_1')
     bonus_data_set(container, 'bonus_st_2')
 
+    local frame = Gui.get_left_element(player, bonus_container)
+    local disp = frame.container['bonus_st_2'].disp.table
+    local n = bonus_gui_pts_needed(player)
+    disp[bonus_gui_control_pts_n_count.name].caption = n
+    local r = tonumber(disp[bonus_gui_control_pts_a_count.name].caption) - n
+    disp[bonus_gui_control_pts_r_count.name].caption = r
+
     apply_bonus(player)
     return container.parent
 end)
