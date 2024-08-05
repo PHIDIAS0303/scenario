469683f12ad39477efc2d23c7a59483a4e8a3f66
PHIDIAS <phidias@aperx.org>
2024-08-05 14:44:37 +0900

    .

--- modules/addons/lawnmower.lua
+++ modules/addons/lawnmower.lua
@@ -1,9 +1,10 @@
---[[-- Commands Module - Lawnmower
+--[[-- Addon Lawnmower
     - Adds a command that clean up biter corpse and nuclear hole
-    @commands Lawnmower
+    @addon Lawnmower
 ]]
 
 local Commands = require 'expcore.commands' --- @dep expcore.commands
+local Event = require 'utils.event' --- @dep utils.event
 require 'config.expcore.command_general_parse'
 
 Commands.new_command('lawnmower', 'Clean up biter corpse, decoratives and nuclear hole')
@@ -30,4 +31,26 @@ Commands.new_command('lawnmower', 'Clean up biter corpse, decoratives and nuclea
 	player.surface.set_tiles(tile_to_do)
 
     return Commands.success
+end)
+
+local function destroy_decoratives(entity, surface, area)
+    if entity.type ~= 'entity-ghost' and entity.type ~= 'tile-ghost' and entity.prototype.selectable_in_game then
+        game.surfaces[surface].destroy_decoratives{area=area}
+    end
+end
+
+Event.add(defines.events.on_built_entity, function(event)
+    destroy_decoratives(event.created_entity, event.created_entity.surface_index, event.created_entity.selection_box)
+end)
+
+Event.add(defines.events.on_robot_built_entity, function(event)
+    destroy_decoratives(event.created_entity, event.created_entity.surface_index, event.created_entity.selection_box)
+end)
+
+Event.add(defines.events.script_raised_built, function(event)
+    destroy_decoratives(event.entity, event.entity.surface_index, event.entity.selection_box)
+end)
+
+Event.add(defines.events.script_raised_revive, function(event)
+    destroy_decoratives(event.entity, event.entity.surface_index, event.entity.selection_box)
 end)
