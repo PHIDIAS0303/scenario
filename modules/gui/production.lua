49799246d17f23f899dbfa176d11f6d998dec1af
PHIDIAS <phidias@aperx.org>
2024-08-05 20:50:45 +0900

    .

--- modules/gui/production.lua
+++ modules/gui/production.lua
@@ -5,8 +5,9 @@ local Gui = require 'expcore.gui' --- @dep expcore.gui
 local Event = require 'utils.event' --- @dep utils.event
 local Roles = require 'expcore.roles' --- @dep expcore.roles
 local config = require 'config.production' --- @dep config.production
+local format_number = require('util').format_number --- @dep util
 
-local elem_filter = {{filter='type', type='resource'}}
+local elem_filter = {{filter='type', type='item'}}
 
 local production_time_scale =
 Gui.element{
@@ -30,63 +31,79 @@ Gui.element(function(_, parent, name)
     return production_set
 end)
 
+--- Display group
+-- @element production_data_group
+local production_data_group =
+Gui.element(function(_definition, parent, i, caption, tooltip)
+    local item = parent.add{
+        type = 'choose-elem-button',
+        elem_type = 'entity',
+        elem_filters = elem_filter,
+        style = 'slot_button'
+    }
+    item.style.width = 64
+
+    local data_set = parent.add{type='flow', direction='vertical', name=Gui.unique_static_name}
+    local disp = Gui.scroll_table(data_set, 360, 2, 'disp')
+
+    local data_1s = disp.add{
+        type = 'label',
+        name = Gui.unique_static_name,
+        caption = '+',
+        style = 'heading_1_label',
+    }
+    data_1s.style.width = 64
+
+    local data_1c = disp.add{
+        type = 'label',
+        name = 'production_' .. i .. '_1_count',
+        caption = '+',
+        style = 'heading_1_label',
+    }
+    data_1c.style.width = 192
+
+    local data_2s = disp.add{
+        type = 'label',
+        name = Gui.unique_static_name,
+        caption = '-',
+        style = 'heading_1_label',
+    }
+    data_2s.style.width = 64
+
+    local data_2c = disp.add{
+        type = 'label',
+        name = 'production_' .. i .. '_2_count',
+        caption = '+',
+        style = 'heading_1_label',
+    }
+    data_2c.style.width = 192
+
+    local data_3s = disp.add{
+        type = 'label',
+        name = Gui.unique_static_name,
+        caption = '=',
+        style = 'heading_1_label',
+    }
+    data_3s.style.width = 64
+
+    local data_3c = disp.add{
+        type = 'label',
+        name = 'production_' .. i .. '_3_count',
+        caption = '+',
+        style = 'heading_1_label',
+    }
+    data_3c.style.width = 192
+end)
+
 --- A vertical flow containing all the production data
 -- @element production_data_set
 local production_data_set =
 Gui.element(function(_, parent, name)
     local production_set = parent.add{type='flow', direction='vertical', name=name}
-    local disp = Gui.scroll_table(production_set, 360, 3, 'disp')
+    local disp = Gui.scroll_table(production_set, 360, 2, 'disp')
 
     for i=1, config.row do
-        disp.add{
-            name = 'production_data_' .. i .. '_0',
-            type = 'choose-elem-button',
-            elem_type = 'entity',
-            elem_filters = elem_filter,
-            style = 'slot_button'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_11',
-            type = 'label',
-            caption = '+',
-            style = 'heading_1_label'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_12',
-            type = 'label',
-            caption = '0',
-            style = 'heading_1_label'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_21',
-            type = 'label',
-            caption = '-',
-            style = 'heading_1_label'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_22',
-            type = 'label',
-            caption = '0',
-            style = 'heading_1_label'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_31',
-            type = 'label',
-            caption = '=',
-            style = 'heading_1_label'
-        }
-
-        disp.add{
-            name = 'production_data_' .. i .. '_32',
-            type = 'label',
-            caption = '0',
-            style = 'heading_1_label'
-        }
+        production_data_group(disp, i)
     end
 
     return production_set
