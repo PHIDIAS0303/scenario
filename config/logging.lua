e4c73830c9a46be259b74a29bb73f1d799eb794d
PHIDIAS <phidias@aperx.org>
2024-08-07 01:33:04 +0900

    .

--- config/logging.lua
+++ config/logging.lua
@@ -2,7 +2,7 @@
 -- @config logging
 
 return {
-    file_name = 'ext/logging.out',
+    file_name = 'log/logging.log',
     rocket_launch_display = {
         [1] = true,
         [2] = true,
@@ -28,16 +28,16 @@ return {
         [10000000] = true
     },
     disconnect_reason = {
-        [defines.disconnect_reason.quit] = 'quit',
-        [defines.disconnect_reason.dropped] = 'dropped',
-        [defines.disconnect_reason.reconnect] = 'reconnect',
-        [defines.disconnect_reason.wrong_input] = 'wrong input',
-        [defines.disconnect_reason.desync_limit_reached] = 'desync limit reached',
-        [defines.disconnect_reason.cannot_keep_up] = 'cannot keep up',
-        [defines.disconnect_reason.afk] = 'afk',
-        [defines.disconnect_reason.kicked] = 'kicked',
-        [defines.disconnect_reason.kicked_and_deleted] = 'kicked and deleted',
-        [defines.disconnect_reason.banned] = 'banned',
-        [defines.disconnect_reason.switching_servers] = 'switching servers'
+        [defines.disconnect_reason.quit] = ' has left the game',
+        [defines.disconnect_reason.dropped] = ' was dropped from the game',
+        [defines.disconnect_reason.reconnect] = ' is reconnecting',
+        [defines.disconnect_reason.wrong_input] = ' was having a wrong input',
+        [defines.disconnect_reason.desync_limit_reached] = ' had desync limit reached',
+        [defines.disconnect_reason.cannot_keep_up] = ' cannot keep up',
+        [defines.disconnect_reason.afk] = ' was afk',
+        [defines.disconnect_reason.kicked] = ' was kicked',
+        [defines.disconnect_reason.kicked_and_deleted] = ' was kicked and deleted',
+        [defines.disconnect_reason.banned] = ' was banned',
+        [defines.disconnect_reason.switching_servers] = ' is switching servers'
     }
 }
--- modules/addons/logging.lua
+++ modules/addons/logging.lua
@@ -6,37 +6,42 @@ local Event = require 'utils.event' --- @dep utils.event
 local config = require 'config.logging' --- @dep config.logging
 local write_json = _C.write_json
 
+local function add_log(data)
+	game.write_file(config.file_name, data .. '\n', true, 0)
+end
+
 Event.add(defines.events.on_rocket_launched, function(event)
     if config.rocket_launch_display[event.rocket.force.rockets_launched] then
-        write_json(config.file_name, {type='rocket_launch', number=event.rocket.force.rockets_launched})
+        add_log(event.rocket.force.rockets_launched .. ' rockets launched')
     end
 end)
 
 Event.add(defines.events.on_pre_player_died, function(event)
     if event.cause then
         if event.cause.type == 'character' then
-            -- pvp
-            write_json(config.file_name, {type='player_died', player=game.players[event.player_index].name, name=game.players[event.cause.player.index].name or 'no-cause'})
+            add_log(game.players[event.player_index].name .. ' died because of ' .. (game.players[event.cause.player.index].name or ' unknown reason'))
 
         else
-            -- cause
-            write_json(config.file_name, {type='player_died', player=game.players[event.player_index].name, name=event.cause.name or 'no-cause'})
+            add_log(game.players[event.player_index].name .. ' died because of ' .. (event.cause.name or ' unknown reason'))
         end
 
     else
-        -- poison damage
-        write_json(config.file_name, {type='player_died', player=game.players[event.player_index].name, name='no-cause'})
+        add_log(game.players[event.player_index].name .. ' died because of unknown reason')
 	end
 end)
 
 Event.add(defines.events.on_research_finished, function(event)
-    write_json(config.file_name, {type='research', name=string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' '), level=event.research.level or 1})
+    if event.by_script then
+        return
+    end
+
+    add_log(string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' at level ' .. (event.research.level - 1) .. ' has been researched')
 end)
 
 Event.add(defines.events.on_player_joined_game, function(event)
-	write_json(config.file_name, {type='join', player=game.players[event.player_index].name})
+    add_log(game.players[event.player_index].name .. ' has joined the game')
 end)
 
 Event.add(defines.events.on_player_left_game, function(event)
-	write_json(config.file_name, {type='leave', player=game.players[event.player_index].name, reason=config.disconnect_reason[event.reason] or 'other'})
+    add_log(game.players[event.player_index].name .. (config.disconnect_reason[event.reason] or config.disconnect_reason[defines.disconnect_reason.quit]))
 end)
