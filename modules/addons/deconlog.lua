0619ec445062a6ce1140506aae93b3ac506d388d
PHIDIAS <phidias@aperx.org>
2024-08-29 13:26:44 +0900

    .

--- modules/addons/deconlog.lua
+++ modules/addons/deconlog.lua
@@ -56,7 +56,7 @@ if config.decon_area then
 			print_to_players(true, player.name .. ' tried to deconstruct the area ' .. pos_to_gps_string(e.area.left_top) .. ' to ' .. pos_to_gps_string(e.area.right_bottom) .. ' that have ' .. format_number(#items) .. ' items, but were not allowed.')
 		end
 
-		add_log(get_secs() .. "," .. player.name .. ",decon_area," .. pos_to_string(e.area.left_top) .. "," .. pos_to_string(e.area.right_bottom))
+		add_log(get_secs() .. ',' .. player.name .. ',decon_area,' .. pos_to_string(e.area.left_top) .. ',' .. pos_to_string(e.area.right_bottom))
 	end)
 end
 
