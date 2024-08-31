2d0001d7ded4c677e05e22cb1c9752983eb56044
PHIDIAS <phidias@aperx.org>
2024-09-01 00:28:13 +0900

    .

--- modules/commands/enemy.lua
+++ modules/commands/enemy.lua
@@ -6,14 +6,14 @@
 local Commands = require 'expcore.commands' --- @dep expcore.commands
 require 'config.expcore.command_general_parse'
 
-Commands.new_command('kill-biters', 'Kill all biters (only)')
+Commands.new_command('kill-biters', {'expcom-enemy.descr-kill'})
 :set_flag('admin_only')
 :register(function(_, _)
     game.forces['enemy'].kill_all_units()
     return Commands.success
 end)
 
-Commands.new_command('remove-biters', 'Remove biters and prevent generation')
+Commands.new_command('remove-biters', {'expcom-enemy.descr-remove'})
 :set_flag('admin_only')
 :add_param('surface', true, 'surface')
 :set_defaults{surface=function(player)
