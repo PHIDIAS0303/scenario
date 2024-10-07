04fc377692e98c767fa66f9fb8856fdbeaf5a962
PHIDIAS <phidias@aperx.org>
2024-10-07 20:33:57 +0900

    .

--- modules/gui/production.lua
+++ modules/gui/production.lua
@@ -139,8 +139,8 @@ Event.on_nth_tick(60, function()
             local item = table[production_prefix .. '_e'].elem_value
 
             if item then
-                local add = math.floor(stat.get_flow_count{name=item, input=true, precision_index=precision_value, count=false} / 6) / 10
-                local minus = math.floor(stat.get_flow_count{name=item, input=false, precision_index=precision_value, count=false} / 6) / 10
+                local add = string.format('%.1f', stat.get_flow_count{name=item, input=true, precision_index=precision_value, count=false} / 60)
+                local minus = string.format('%.1f', stat.get_flow_count{name=item, input=false, precision_index=precision_value, count=false} / 60)
                 local sum = add - minus
 
                 table[production_prefix .. '_1'].caption = format_n(add)
