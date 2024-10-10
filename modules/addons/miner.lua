1bcc6fea8483216c061db71ae83eb5ec017319e5
PHIDIAS <phidias@aperx.org>
2024-10-10 17:01:55 +0900

    .

--- modules/addons/miner.lua
+++ modules/addons/miner.lua
@@ -196,7 +196,7 @@ command/miner-check-force
 
 Event.on_nth_tick(60, function(event)
     for k, q in pairs(miner_data.queue) do
-        if (not q.e) or (not q.e.valid) then
+        if q.e and q.e.valid then
             if event.tick >= q.t then
                 q.e.order_deconstruction(q.e.force)
                 table.remove(miner_data.queue, k)
